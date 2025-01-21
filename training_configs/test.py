e6c1d587c3dc81ce736dc05b3bb7d482dfc61c4e
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2025-01-21 20:46:40 +0100

    configs

--- training_configs/test.py
+++ training_configs/test.py
@@ -1,7 +1,14 @@
 import yaml
 
 # Load baseline configuration
-with open("training_configs/baseline.yml", "r") as file:
-    baseline_config = yaml.safe_load(file)
+with open("training_configs/config1.yml", "r") as file:
+    config = yaml.safe_load(file)
 
-print(baseline_config)
+print(config)
+
+wandb_config = config["wandb_config"]
+
+import wandb
+
+wandb.init(project="config_example2",
+           config=config)
